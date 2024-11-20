3f1990f1facf392e28d336456683baea1e36ea64
jichen <jichenssg@gmail.com>
2024-11-20 15:54:22 +1100

    checkout init

---
+++ app/checkout/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cloudwego/kitex/pkg/klog"
+	"github.com/cloudwego/kitex/server"
+	checkout "github.com/jichenssg/tikmall/gen/kitex_gen/checkout/checkoutservice"
+	"github.com/kanhai-syd/hailog/logging"
+	"github.com/natefinch/lumberjack"
+
+	"github.com/jichenssg/tikmall/app/checkout/config"
+	serversuite "github.com/jichenssg/tikmall/app/common/serversuite"
+
+	"github.com/jichenssg/tikmall/app/common/client"
+	"github.com/jichenssg/tikmall/app/common/obs"
+)
+
+func main() {
+	config.GetConf()
+
+	obs.InitTracer(config.GetConf().Server.Name)
+
+	obs.InitMetrics(
+		config.GetConf().Server.Name,
+		config.GetConf().Server.Host,
+		config.GetConf().Metrics.Port,
+		fmt.Sprintf("%v:%v", config.GetConf().Consul.Host, config.GetConf().Consul.Port),
+	)
+
+	obs.InitLog(&lumberjack.Logger{
+		Filename:   config.GetConf().Kitex.LogFileName,
+		MaxSize:    config.GetConf().Kitex.LogMaxSize,
+		MaxBackups: config.GetConf().Kitex.LogMaxBackups,
+		MaxAge:     config.GetConf().Kitex.LogMaxAge,
+	}, config.LogLevel())
+
+	// mysql.Init(
+	// 	config.GetConf().Mysql.Dsn,
+	// 	config.GetConf().Mysql.User,
+	// 	config.GetConf().Mysql.Password,
+	// 	config.GetConf().Mysql.Host,
+	// 	config.GetConf().Mysql.Port,
+	// 	config.GetConf().Mysql.Database,
+	// )
+	// mysql.GetDB().AutoMigrate()
+
+	client.Init(
+		config.GetConf().Server.Name,
+		fmt.Sprintf("%v:%v", config.GetConf().Consul.Host, config.GetConf().Consul.Port),
+		fmt.Sprintf("%v:%v", config.GetConf().Telemetry.Host, config.GetConf().Telemetry.Port),
+	)
+
+	klog.Infof("Starting service %v", config.GetConf().Server.Name)
+
+	kitexInit()
+}
+
+func kitexInit() {
+	svr := checkout.NewServer(
+		new(CheckoutServiceImpl),
+		server.WithSuite(
+			serversuite.CommonServerSuite{
+				ServiceName:      config.GetConf().Server.Name,
+				Host:             config.GetConf().Server.Host,
+				Port:             config.GetConf().Server.Port,
+				RegistryEndpoint: fmt.Sprintf("%v:%v", config.GetConf().Consul.Host, config.GetConf().Consul.Port),
+				OtelEndpoint:     fmt.Sprintf("%v:%v", config.GetConf().Telemetry.Host, config.GetConf().Telemetry.Port),
+			},
+		),
+	)
+
+	err := svr.Run()
+
+	if err != nil {
+		logging.Fatal(err)
+	}
+}
